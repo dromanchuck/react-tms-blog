c4b7764ee850e59f58cb984b15b026cd633033ca
Dzmitry Ramanchuk <dmitryromanchuck@gmail.com>
2022-07-13 18:33:35 +0300

    remove all

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css";
-
-export function Button({ text, type = "primary", onClick }) {
-  return (
-    <button className={styles[type]} onClick={onClick}>
-      {text}
-    </button>
-  );
-}
--- src/redux/types.ts
+++
@@ -1,9 +0,0 @@
-export interface IPost {
-  id: number;
-  image: string;
-  text: string;
-  date: string;
-  lesson_num: number;
-  title: string;
-  author: number;
-}
